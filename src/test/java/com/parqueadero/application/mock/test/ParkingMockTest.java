796d6407032d2214fe1d64cb53c4d6d1b7316380
jalozanot <jalozanot@gmail.com>
2019-02-14 00:19:43 -0500

    validcion de salida moto

--- src/test/java/com/parqueadero/application/mock/test/ParkingMockTest.java
+++ src/test/java/com/parqueadero/application/mock/test/ParkingMockTest.java
@@ -38,54 +38,5 @@ public class ParkingMockTest {
 		
 	}
 	
-//	@Test
-//	public void obtenerDiaLunes()  {
-//		when(bussines.obtenerDia()).thenReturn(ConstantesVal.LUNES);
-//		String dia = bussines.obtenerDia();
-//		Assert.assertTrue(ConstantesVal.LUNES.equals(dia));
-//	}
-//	
-//	@Test
-//	public void obtenerDiaMartes()  {	
-//		when(bussines.obtenerDia()).thenReturn(ConstantesVal.MARTES);
-//		String dia = bussines.obtenerDia();
-//		Assert.assertTrue(ConstantesVal.MARTES.equals(dia));
-//	}
-//	
-//	@Test
-//	public void obtenerDiaMiercoles()  {	
-//		when(bussines.obtenerDia()).thenReturn(ConstantesVal.MIERCOLES);
-//		String dia = bussines.obtenerDia();
-//		Assert.assertTrue(ConstantesVal.MIERCOLES.equals(dia));
-//	}
-//	
-//	@Test
-//	public void obtenerDiaJueves()  {	
-//		when(bussines.obtenerDia()).thenReturn(ConstantesVal.JUEVES);
-//		String dia = bussines.obtenerDia();
-//		Assert.assertTrue(ConstantesVal.JUEVES.equals(dia));
-//	}
-//	
-//	@Test
-//	public void obtenerDiaViernes()  {	
-//		when(bussines.obtenerDia()).thenReturn(ConstantesVal.VIERNES);
-//		String dia = bussines.obtenerDia();
-//		Assert.assertTrue(ConstantesVal.VIERNES.equals(dia));
-//	}
-//	
-//	@Test
-//	public void obtenerDiaSabado()  {	
-//		when(bussines.obtenerDia()).thenReturn(ConstantesVal.SABADO);
-//		String dia = bussines.obtenerDia();
-//		Assert.assertTrue(ConstantesVal.SABADO.equals(dia));
-//	}
-//	
-//	@Test
-//	public void obtenerDiaDomingo()  {	
-//		when(bussines.obtenerDia()).thenReturn(ConstantesVal.DOMINGO);
-//		String dia = bussines.obtenerDia();
-//		Assert.assertTrue(ConstantesVal.DOMINGO.equals(dia));
-//	}
-	
 }
 
